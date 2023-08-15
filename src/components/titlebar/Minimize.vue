759d3729bcd0eda90e0a29110ac5591e4fc1caaa
Mathias Mantai <mantai@meuser.it>
2023-08-15 10:35:08 +0200

    chore: removing xmlns attribute from svg tag

--- src/components/titlebar/Minimize.vue
+++ src/components/titlebar/Minimize.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg class="fill-gray-500 group-hover:fill-black" width="16" height="16" viewBox="0 0 16 16"  xmlns="http://www.w3.org/2000/svg">
+    <svg class="fill-gray-500 group-hover:fill-black" width="16" height="16" viewBox="0 0 16 16">
         <rect x="2" y="7" width="12" height="2" />
     </svg>
 </template>
--- src/components/titlebar/UnMaximize.vue
+++ src/components/titlebar/UnMaximize.vue
@@ -1,9 +1,9 @@
 <template>
-    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
         <rect x="2.5" y="4.5" width="9" height="9" stroke="black"/>
         <rect x="13" y="2" width="1" height="11" fill="black"/>
         <rect x="3" y="2" width="11" height="1" fill="black"/>
         <rect x="3.25" y="3.25" width="0.5" height="0.5" stroke="black" stroke-width="0.5"/>
         <rect x="12.25" y="12.25" width="0.5" height="0.5" stroke="black" stroke-width="0.5"/>
-</svg>
+    </svg>
 </template>
